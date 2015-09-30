a97f1f5fd4d464b869be8e55099d4e24477ae1f5
Pedro Russo <pedrorusso@pedros-mac.local>
2015-09-30 19:45:35 +0100

    Initial Commit

---
+++ FlowlinerTests/FlowlinerTests.swift
@@ -0,0 +1,36 @@
+//
+//  FlowlinerTests.swift
+//  FlowlinerTests
+//
+//  Created by Pedro Russo on 30/09/15.
+//  Copyright © 2015 Pedro Russo. All rights reserved.
+//
+
+import XCTest
+@testable import Flowliner
+
+class FlowlinerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
